5fc13344e0814450b5da1da51801173b4286234d
Jordan Joe Cooper <32134629+jordanjoecooper@users.noreply.github.com>
2023-12-18 14:12:27 +0000

    Create hello.cob
    Add Cobol example.

---
+++ hello.cob
@@ -0,0 +1,10 @@
+       IDENTIFICATION DIVISION.
+       PROGRAM-ID. Hello.
+       PROCEDURE DIVISION.
+           DISPLAY 'Hello, World!'.
+           STOP RUN.
+
+* Install cobol compiler such as GnuCOBOL on windows
+* Open terminal and go to the direct with this file
+* Compile the file using GnuCOBOL with cobc -x -o hello hello.cob
+* Run by executing the following in terminal ./hello
