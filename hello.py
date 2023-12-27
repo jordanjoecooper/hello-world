24072b9e45de473684ad47acda736021764763b4
Jordan Cooper <jordan.cooper@moneyhub.com>
2023-12-27 17:32:38 +0000

    Add python

---
+++ hello.py
@@ -0,0 +1,5 @@
+print("Hello World!")
+
+# Open your command line and run the script by entering: python hello.py
+# Most modern computers come with a version of Python installed, so you should see the output right away.
+# If you don't you need to install Python on your computer and repeat the first comment above.
