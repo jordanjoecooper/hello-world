d2eebb8981fbdc609865826c93ddb371b44a8bc7
Jordan Joe Cooper <32134629+jordanjoecooper@users.noreply.github.com>
2025-07-15 15:31:36 +0100

    Add nodejs example

---
+++ hello.js
@@ -0,0 +1,18 @@
+/*
+1. Ensure Node.js is installed:
+       node --version
+       
+2. Create a file called 
+        hello.js
+        
+3. Add the following to the file and save it
+      console.log("Hello, world!");
+      
+4. From a terminal navigate to the file and run
+      node hello.js
+
+You should see:
+       Hello, world!
+*/
+
+console.log("Hello, world!");
