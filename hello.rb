b2d35ab972aa5f0c94156188d44f262a77728e06
Jordan Joe Cooper <32134629+jordanjoecooper@users.noreply.github.com>
2025-07-15 15:26:25 +0100

    Add install and run instructions for Ruby example

--- hello.rb
+++ hello.rb
@@ -1,1 +1,12 @@
 puts "Hello World"
+
+# 1. Check Ruby version (in terminal)
+#    ruby -v
+# If it is missing install it from https://www.ruby-lang.org/en/
+# 2. Create a new Ruby file (in terminal)
+#    touch hello.rb
+# 3. Open the file in a text editor and add:
+# puts "Hello, world!"
+# 4. Save the file.
+# 5. Run the Ruby file from terminal:
+#    ruby hello.rb
