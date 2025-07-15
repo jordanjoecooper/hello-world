f3dddd1aec7aee884907898e78831ce5ff5bc1fd
Jordan Joe Cooper <32134629+jordanjoecooper@users.noreply.github.com>
2025-07-15 15:28:40 +0100

    Create hello.c example

---
+++ hello.c
@@ -0,0 +1,21 @@
+#include <stdio.h>
+
+int main() {
+    printf("Hello, world!\n");
+    return 0;
+}
+
+/*
+1. Ensure you have a C compiler (e.g. gcc).
+   Check with:
+       gcc --version
+
+2. Compile the file:
+       gcc hello.c -o hello
+
+3. Run the output:
+       ./hello
+
+You should see:
+    Hello, world!
+*/
